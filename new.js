7aca1393b4be96643fa24cb652e4cbe71d96a4e9
varenyamsingh <130786405+varenyamsingh@users.noreply.github.com>
2024-01-09 22:00:40 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,2 @@
+console.log("Hello World")
+console.log("Code is running...")
---
+++ script.js
@@ -0,0 +1,16 @@
+alert("Hello User")
+
+console.log("Hey I am console")
+
+var a = prompt("Enter your number: ")
+var isTrue = confirm("Are you sure you have to burn your computer?")
+if(isTrue){
+    console.log("Computer is preparing to blast...")
+}
+else{
+    console.log("Computer is not blasting")
+}
+console.log("Your Number is " + a)
+
+document.title = "Hey I am good"
+document.body.style.backgroundColor = "red"
